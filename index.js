0d13ca9e26d3b39447bd1c6bae34d603e1efc87d
Thomas Ngo <email@thomasngo.ca>
2024-01-03 22:03:37 -0800

    Implement client slash command listener functionality

--- index.js
+++ index.js
@@ -40,7 +40,22 @@ client.once(Events.ClientReady, readyClient => {
 client.login(token);
 
 // Create client listener for slash commands only
-client.on(Events.InteractionCreate, interaction => {
+client.on(Events.InteractionCreate, async interaction => {
     if (!interaction.isChatInputCommand()) return;
-    console.log(interaction);
+    const command = interaction.client.commands.get(interaction.commandName);
+
+    if (!command) {
+        console.error('No command matching $(interaction.commandName) was found.');
+        return;
+    }
+
+    try {
+        await command.execute(interaction);
+    } catch (error) {
+        if (interaction.replied || interaction.deffered) {
+            await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
+        } else {
+            await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+        }
+    }
 });
