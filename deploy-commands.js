ddc0440d265aa71710b2412befa746d6fefbc412
Thomas Ngo <email@thomasngo.ca>
2024-01-04 22:55:57 -0800

    initial commit with comments of planned implementation

---
+++ deploy-commands.js
@@ -0,0 +1,14 @@
+// const { REST, Routes } = require('discord.js');
+// const { clientId, guildId, token } = require('./config.json');
+// const fs = require('node:fs');
+// const path = require('node:path');
+
+// Grab all command folders paths
+
+// loop through folders and grab all command files' paths
+
+// loop through each file and get each commands's data in json and store in array
+
+// construct instance of rest module
+
+// deploy commands with rest module
