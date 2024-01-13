215b86926ab6be82c0cc4b8abd206b232529031e
Thomas Ngo <email@thomasngo.ca>
2024-01-13 13:35:00 -0800

    implement command file path parser and get output as json

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,13 +1,28 @@
-// const { REST, Routes } = require('discord.js');
-// const { clientId, guildId, token } = require('./config.json');
-// const fs = require('node:fs');
-// const path = require('node:path');
+const { REST, Routes } = require('discord.js');
+const { clientId, guildId, token } = require('./config.json');
+const fs = require('node:fs');
+const path = require('node:path');
 
 // Grab all command folders paths
+const commands = []
+const foldersPath = path.join(__dirname, 'commands');
+const commandFolders = fs.readdirSync(foldersPath);
 
 // loop through folders and grab all command files' paths
-
-// loop through each file and get each commands's data in json and store in array
+for (const folder of commandFolders) {
+    const commandsPath = path.join(foldersPath, folder);
+    const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('js.'));
+    // loop through each file and get each commands's data in json and store in array
+    for (const file of commandFiles) {
+        const filePath = path.join(commandsPath, file);
+        const command = require(filePath);
+        if ('data' in command && 'execute' in command) {
+            commands.push(command.data.toJSON());
+        } else {
+            console.log(`[WARNING] THe command at ${filePath} is missing a required "data" or "execute" property`);
+        }
+    }
+}
 
 // construct instance of rest module
 
